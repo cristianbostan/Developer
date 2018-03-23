78c9a0677a112b0cbb0513bb3df1bcb4cbd2d171
cristianbostan <christian.bostan@gmail.com>
2018-03-23 21:59:51 +0200

    Cristian - begin chapter 8

--- Newbie_experiments/Ex_Files_FoP_Fun/Exercise Files/ch05/Parameters/script.js
+++ Newbie_experiments/Ex_Files_FoP_Fun/Exercise Files/ch05/Parameters/script.js
@@ -1,8 +1,12 @@
-function addTwoNumbers() {
-   	var a = 5;
-	var b = 10;
+function addTwoNumbers(a,b) {
 	var result = a + b;
-	alert(result);
+	return result;
+  //return exits the function and passes the data to whoever called it
 }
 
-addTwoNumbers();
+var x = addTwoNumbers(5,10);
+//the result is passed to var x
+alert(x);
+//x is displayed in a pop-up
+addTwoNumbers(500,100);
+addTwoNumbers(7,-2323);
