3d5aefb67535284c536afcac99e9109d71783e2b
cristianbostan <christian.bostan@gmail.com>
2018-03-25 22:07:24 +0300

    Cristian - begin chapter 10

--- Newbie_experiments/Ex_Files_FoP_Fun/Exercise Files/ch08/Array iteration/script.js
+++ Newbie_experiments/Ex_Files_FoP_Fun/Exercise Files/ch08/Array iteration/script.js
@@ -1,12 +1,11 @@
-var myArray = [500,500,500,500,500];
+var myArray = [500,500,500,500,500,13,436546];
 
 var total = 0;
 
 for ( var i = 0 ; i < myArray.length ; i++ ) {
-    // add the current element to the total	
+    // add the current element to the total
     total = total + myArray[i];
 }
 
 // after we're done with the loop
 alert("The total is: " + total);
-
