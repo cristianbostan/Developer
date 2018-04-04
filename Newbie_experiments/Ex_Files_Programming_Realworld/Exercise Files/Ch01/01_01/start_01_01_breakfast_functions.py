9d399b72ea5b0a574133f06ae2eaa37aaa47d441
cristianbostan <christian.bostan@gmail.com>
2018-04-04 18:04:58 +0300

    Cristian - finish ch1 lesson3

--- Newbie_experiments/Ex_Files_Programming_Realworld/Exercise Files/Ch01/01_01/start_01_01_breakfast_functions.py
+++ Newbie_experiments/Ex_Files_Programming_Realworld/Exercise Files/Ch01/01_01/start_01_01_breakfast_functions.py
@@ -1,8 +1,13 @@
 """ A Functional Breakfast """
 
-print('Mixing the ingredients')
-print('Pouring the mixture into a frying pan')
-print('Cooking the first side')
-print('Flipping it!')
-print('Cooking the other side')
-omelette = 'a tasty omelette'
+def make_omelette():
+    print('Mixing the ingredients')
+    print('Pouring the mixture into a frying pan')
+    print('Cooking the first side')
+    print('Flipping it!')
+    print('Cooking the other side')
+    omelette = 'a tasty omelette'
+    return omelette
+
+omelette1 = make_omelette()
+omelette2 = make_omelette()
